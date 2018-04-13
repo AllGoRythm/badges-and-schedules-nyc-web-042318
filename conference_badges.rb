0a6ac72c58738de1c1ffb866980e301b0ad6dff6
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-13 20:21:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def batch_badge_creator(message)
   container = []
 
   i = 0
-  while message.length <= 30
+  while message.length >= 30
     container.push("Hello, my name is #{message}.")
     i += 1
   end
